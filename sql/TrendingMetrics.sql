fd2eb629be23eda0d55f295f1fdc561f824ca8b3
Harley Franco <hdfranco@usc.edu>
2024-11-26 13:55:06 -0800

    Added table creation sql stuff

---
+++ sql/TrendingMetrics.sql
@@ -0,0 +1,8 @@
+CREATE TABLE trending_scores (
+	metric_id INT PRIMARY KEY AUTO_INCREMENT,
+	spot_id INT NOT NULL,
+	total_visits INT NOT NULL,
+	trending_metrics double NOT NULL,
+	average_rating double NOT NULL,
+	FOREIGN KEY (spot_id) REFERENCES StudySpots(spot_id)
+);
