33be6350e571de0816996f1fe33c94da2a3fb788
Claire Yang <clairey@usc.edu>
2024-12-08 15:36:46 -0800

    editing getbyid in allstudyspotscontroller

--- src/main/java/studyspots/AllStudySpots/AllStudySpotsController.java
+++ src/main/java/studyspots/AllStudySpots/AllStudySpotsController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -47,8 +48,8 @@ public class AllStudySpotsController {
 		return this.allStudySpotsRepository.findByName(name);
 	}
 	
-	@GetMapping("/getbyid")
-	public @ResponseBody StudySpot getStudySpotById(@RequestParam int id) {
+	@GetMapping("/getbyid/{id}")
+	public @ResponseBody StudySpot getStudySpotById(@PathVariable int id) {
 		return this.allStudySpotsRepository.findById(id);
 	}
 
